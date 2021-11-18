7480bd9d5e42b1ab50323b2e66b5af7ae42324aa
JeremiahISamuelMMS <79987177+JeremiahISamuelMMS@users.noreply.github.com>
2021-11-18 16:55:30 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,8 +7,33 @@ LogRocket.init('5i2sox/chat-app');
 const projectID = '889e8a88-ce27-4629-8051-3f85c29181e1';
 const App = () => {
   if (!localStorage.getItem('username')) return <LoginForm />;
+  
+  const showNotification = (message) => {
+    let granted = false;
+		if (Notification.permission === 'granted') {
+			granted = true;
+		} else if (Notification.permission !== 'denied') {
+			let permission = await Notification.requestPermission();
+			granted = permission === 'granted' ? true : false;
+		}
+  if (granted) {
+		// create a new notification
+		const notification = new Notification('New Message', {
+			body: `${message.text} Click To view More or Reply`,
+			icon: 'https://stem-club-chat.netlify.app/favicon.ico'
+		});
+		// close the notification after 10 seconds
+		setTimeout(() => {
+			notification.close();
+		}, 10 * 1000);
 
+		// navigate to a URL when clicked
+		notification.addEventListener('click', () => {
 
+			window.open('https://stem-club-chat.netlify.app/', '_blank');
+		});
+	}
+}
 
   return (
     <ChatEngine
@@ -16,15 +41,7 @@ const App = () => {
       projectID={projectID}
       userName={localStorage.getItem('username')}
       userSecret={localStorage.getItem('password')}
-      onNewMessage={(data, message) => fetch('https://events.sendpulse.com/events/id/e323256cfeb643398a9d06b613f2b1ce/7873933', {
-        method: 'POST',
-        body: JSON.stringify({
-          "email": 'jr.isamuel08@gmail.com' ,
-          "phone": '7327886322',
-          "sender": message.sender_username,
-          "message": message.text,
-        })
-      }).then(response => response.json()).then(console.log).then(new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play())
+      onNewMessage={(message) => showNotification(message).then(console.log).then(new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play())
       }
     />
     
